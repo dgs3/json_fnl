b7e73bf03e6103727ad930cd61ab8a076d5281a4
David Sayles <david.sayles@mongodb.com>
2021-08-09 19:06:22 +0000

    Fill in some build stuff from pypi

--- setup.py
+++ setup.py
@@ -8,21 +8,18 @@ if sys.version_info < (3, 3):
 
 setup(
     name='yajl',
-    version='0.1',
+    version='0.0.1',
     license='BSD',
     url='https://github.com/dgs3/yajl',
     author='Dave Sayles',
     author_email='sayles.dave@gmail.com',
     description='Module to discover and lint json files.',
     classifiers=[
-        'Environment :: Web Environment',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: BSD License',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.3',
-        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
         'Topic :: Software Development :: Libraries :: Python Modules'
     ],
     packages=["yajl"],
